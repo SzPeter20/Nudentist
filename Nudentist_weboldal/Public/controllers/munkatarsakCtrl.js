d1ef6580150806c153c8913d6f805621ff1a9644
zamboilles21 <zamboilles@turr.hu>
2023-02-10 09:03:12 +0100

    munkatarsak folyamatban

--- Nudentist_weboldal/Public/controllers/munkatarsakCtrl.js
+++ Nudentist_weboldal/Public/controllers/munkatarsakCtrl.js
@@ -2,8 +2,10 @@ app.controller('munkatarsakCtrl', function($scope, DB, $rootScope,) {
     $scope.munkatarsak=[];
     $scope.munkas={};
     $scope.db = [];
-    DB.selectAll('users').then(function(res) {
+    DB.selectAll('orvosok').then(function(res) {
         $scope.munkatarsak= res.data;
-        
+        for (let i = 0; i < $scope.munkatarsak.length; i++) {
+            $scope.db[i] = 1;
+        }
     });
 });
