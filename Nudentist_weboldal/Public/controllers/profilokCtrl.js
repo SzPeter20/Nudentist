a05a4ac88ed4e22dfe47e3a066d4e2431e5c15a2
SzaboPeter20 <2001szabopeter@gmail.com>
2023-02-17 09:36:04 +0100

    doktor profil controller

---
+++ Nudentist_weboldal/Public/controllers/profilokCtrl.js
@@ -0,0 +1,39 @@
+app.controller('profilokCtrl', function($scope, DB, $rootScope,){
+
+    $scope.uzenetek;
+    $scope.comms;
+    $scope.stringrating;
+    $scope.pointrating;
+    $scope.message;
+    $scope.csillagok;
+
+    DB.selectAll("uzenetek").then(function(res){
+        $scope.uzenetek=res.data;
+    })
+
+    $scope.rate= function(){
+        if($scope.stringrating.length>500){
+            alert("Kérjük értékelése tartalmazzon maximum 500 karaktert")
+        }else{
+                DB.update("uzenetek")
+
+            if(false){
+                DB.insert("ertekelesek",$scope.comms).then(function(res){
+                
+                })
+            }
+            }
+    }
+    $scope.message= function(){
+        if($scope.message.length>500){
+            alert("Kérjük üzenete tartalmazzon maximum 500 karaktert")
+        }else{
+            DB.insert("uzenetek",$scope.comms).then(function(res){
+
+            })
+        }
+    }
+    $scope.pointrate= function(){
+        
+    }
+});
