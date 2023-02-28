42b647be9c3757cd970caa820bb1470ae640727a
SzaboPeter20 <2001szabopeter@gmail.com>
2023-02-28 09:13:09 +0100

    kezelés location path

--- Nudentist_weboldal/Public/controllers/kezelesekCtrl.js
+++ Nudentist_weboldal/Public/controllers/kezelesekCtrl.js
@@ -1,29 +1,24 @@
-app.controller('kezelesekCtrl', function($scope, DB, $rootScope,) {
+app.controller('kezelesekCtrl', function($scope, DB, $rootScope,$location) {
 
-    $scope.chosenKezeles=[];
+    $scope.chosenKezeles={};
     $scope.kategoriak=[];
     $scope.kezelesek=[];
 
     DB.selectAll('kezelesek').then(function(res) {
         $scope.kezelesek= res.data;
-        for (let i = 0; i < $scope.kezelesek.length; i++) {
-            $scope.db[i] = 1;
-        }
+        
     });
     DB.selectDistinct('kezelesek','kategoria').then(function(res){
         $scope.kategoriak=res.data;
     })
 
+    $scope.chooseKezeles=function(kezeles){
+        console.log(kezeles)
+        $scope.chosenKezeles=kezeles;
+        $location.path('/kezeles')
+    }
     
     $scope.cheatsheet=function(){
-        DB.select('kezelesek','ID',1).then(function(res){
-            console.log(res.data)
-        })
-    }
-    $scope.idtransfer=function(id){
-        DB.select('kezelesek','ID',id).then(function(res){
-            $scope.chosenKezeles=res.data;
-        })
         console.log($scope.chosenKezeles)
     }
 });
