790de465b90218ad5382be7494c05bb051c3122f
zamboilles21 <zamboilles@turr.hu>
2023-04-12 11:35:16 +0200

    Időpontok javítások

--- Tervek/SQL/nudentist_terv.sql
+++ Tervek/SQL/nudentist_terv.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2023. Ápr 03. 13:54
+-- Létrehozás ideje: 2023. Ápr 12. 11:34
 -- Kiszolgáló verziója: 10.4.6-MariaDB
 -- PHP verzió: 7.3.8
 
@@ -91,7 +91,8 @@ INSERT INTO `idopontok` (`ID`, `orvosID`, `idopont`, `datum`, `paciensID`, `nev`
 (27, 4, '13:07:00', '2023-03-29', 6, 'Zámbó Illés', 'zamboilles@gmail.com', '+36205988683', 'Elfogadva'),
 (28, 4, '12:59:00', '2023-04-19', 6, 'Zámbó Illés', 'zamboilles@gmail.com', '+36205988683', 'Elfogadásra vár'),
 (29, 4, '12:01:00', '2023-04-01', 6, 'Zámbó Illés', 'zamboilles@gmail.com', '+36205988683', 'Elfogadásra vár'),
-(30, 4, '08:01:00', '2023-04-01', 6, 'Zámbó Illés', 'zamboilles@gmail.com', '+36205988683', 'Elfogadásra vár');
+(30, 4, '08:01:00', '2023-04-01', 6, 'Zámbó Illés', 'zamboilles@gmail.com', '+36205988683', 'Elfogadásra vár'),
+(31, 1, '11:28:00', '2023-03-29', 6, 'Zámbó Illés', 'zamboilles@gmail.com', '0691132401', 'Elfogadásra vár');
 
 -- --------------------------------------------------------
 
@@ -105,21 +106,22 @@ CREATE TABLE `kezelesek` (
   `ar` int(11) NOT NULL,
   `leiras` varchar(200) COLLATE utf8_hungarian_ci NOT NULL,
   `kategoria` varchar(50) COLLATE utf8_hungarian_ci NOT NULL,
-  `kep` varchar(100) COLLATE utf8_hungarian_ci DEFAULT NULL
+  `kep` varchar(100) COLLATE utf8_hungarian_ci DEFAULT NULL,
+  `logo` varchar(250) COLLATE utf8_hungarian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
 --
 -- A tábla adatainak kiíratása `kezelesek`
 --
 
-INSERT INTO `kezelesek` (`ID`, `neve`, `ar`, `leiras`, `kategoria`, `kep`) VALUES
-(1, 'PRoba', 5000, 'proba', 'probakategoria', 'semmi'),
-(2, 'Periapikális röntgen', 10000, 'Az ún. periapikális (foggyökércsúcs körüli) röntgenfelvétellel a fog koronájának legfelső pontjától egészen a gyökér legalsó pontjáig mindent láthatunk, a fogat megtámasztó csontállománnyal együtt.', 'rontgen', 'peri.jpg'),
-(3, 'Korona', 5000, 'desc', 'potlas', 'Korona.jpg'),
-(4, 'Ideiglenes fogtömés', 3500, 'desc', 'potlas', 'fogtomes-utan.jpg'),
-(5, 'Nyitott kürett foganként', 45000, 'desc', 'parodontologia', 'tasakKezeles.png'),
-(6, 'Csontpótlás', 49500, 'desc', 'szajsebeszet', 'csontpot.png'),
-(7, 'Láthatatlan fogszabályozás', 299900, 'desc', 'lathatatlanok', 'lathatatlan.jpg');
+INSERT INTO `kezelesek` (`ID`, `neve`, `ar`, `leiras`, `kategoria`, `kep`, `logo`) VALUES
+(1, 'Próba', 5000, 'proba', 'Próbakategória', 'semmi', 'fa-solid fa-tooth'),
+(2, 'Periapikális röntgen', 10000, 'Az ún. periapikális (foggyökércsúcs körüli) röntgenfelvétellel a fog koronájának legfelső pontjától egészen a gyökér legalsó pontjáig mindent láthatunk, a fogat megtámasztó csontállománnyal együtt.', 'Röntgen', 'peri.jpg', 'bi bi-radioactive'),
+(3, 'Korona', 5000, 'desc', 'Pótlás', 'Korona.jpg', 'bi bi-clipboard-heart'),
+(4, 'Ideiglenes fogtömés', 3500, 'desc', 'Pótlás', 'fogtomes-utan.jpg', 'bi bi-cloud-upload-fill'),
+(5, 'Nyitott kürett foganként', 45000, 'desc', 'Parodontológia', 'tasakKezeles.png', 'fa-solid fa-tooth'),
+(6, 'Csontpótlás', 49500, 'desc', 'Szájsebészet', 'csontpot.png', 'bi bi-display-fill'),
+(7, 'Láthatatlan fogszabályozás', 299900, 'desc', 'Láthatatlanok', 'lathatatlan.jpg', 'fa-solid fa-tooth');
 
 -- --------------------------------------------------------
 
@@ -149,10 +151,10 @@ CREATE TABLE `orvosok` (
 --
 
 INSERT INTO `orvosok` (`ID`, `nev`, `email`, `password`, `bemutatkozas`, `szakterulet`, `tanulmanyok`, `idopontID`, `kezelesekID`, `telefonszam`, `kep`, `last`, `reg`, `jogok`) VALUES
-(1, 'Kiss Tamás', 'kisstamas@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', 'Kiss Tamás profi fogorvos', 'Gyermek fogorvos', 'Fogorvos egyetem', 1, 1, '+36205365214', 'IbrahimLajos', '2023-03-29 10:13:18', '0000-00-00 00:00:00', 'doktor'),
+(1, 'Kiss Tamás', 'kisstamas@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', 'Kiss Tamás profi fogorvos', 'Gyermek fogorvos', 'Fogorvos egyetem', 1, 1, '+36205365214', 'IbrahimLajos', '2023-04-04 08:42:18', '0000-00-00 00:00:00', 'doktor'),
 (2, 'Tamás Klára', 't.klara@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', 'Tamás Klára másik fogorvos', 'fogkő', 'Fogorvos egyetem', 3, 1, '06323512612', 'LajosneKlara', '2023-03-06 12:54:03', '0000-00-00 00:00:00', 'doktor'),
 (3, 'Ifjabb Kiss Pista Tamás ', 'IGFPN@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', 'Tamás a legprofibb fogszabályzókkal foglalkozó orvos', 'Fogkőfényesítő', 'Fogorvos egyetem', 1, 3, '616546462484', 'Asztalossimon', '2023-03-06 12:54:03', '0000-00-00 00:00:00', 'doktor'),
-(4, 'Kerék Tomázia Éva ', 'k.teve@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', 'Világelső fogorvos', 'Minden is', 'Fogorvos egyetem', 2, 2, '+362045963211', 'kerektomazia', '2023-04-03 13:07:39', '0000-00-00 00:00:00', 'doktor'),
+(4, 'Kerék Tomázia Éva ', 'k.teve@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', 'Világelső fogorvos', 'Minden is', 'Fogorvos egyetem', 2, 2, '+362045963211', 'kerektomazia', '2023-04-04 08:31:36', '0000-00-00 00:00:00', 'doktor'),
 (5, 'Zámbó Ilona', 'zamboilona89@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', 'Sziasztok ', 'Szájhigénia', 'Fogorvos egyetem', 1, 2, '0696969696', 'zamboilona', '2023-03-14 09:20:40', '2023-03-20 12:31:00', 'doktor'),
 (6, 'Szomorú Ágnes', 'dr.szomoruagnes@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', 'Sziasztok', 'Fogtechnikus', 'Fogorvos egyetem', 2, 3, '0620584212', 'szomoruagnes', '2023-03-14 09:27:22', '2023-03-13 14:09:00', 'doktor'),
 (7, 'Simonfi Tamás', 'simifi@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', 'Sziasztok', 'Általános fogorvos\r\n', 'Fogorvos egyetem', 4, 5, '0620458598', 'SimonfiTamas', '2023-03-14 09:32:17', '2023-03-13 14:19:00', 'doktor'),
@@ -189,11 +191,11 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`ID`, `nev`, `email`, `password`, `telefonszam`, `jogok`, `reg`, `last`, `kedvencekID`, `kep`) VALUES
 (1, 'Próba Pista', 'probapista@nudentits.com', 'Turr513A', '+3620696969', 'user', '2023-02-02', '2023-02-03', 1, 'admin'),
-(3, 'Lajos', 'Lajos@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', '0694251567428541', 'user', '2023-02-18', '2023-04-03', NULL, 'ramona'),
+(3, 'Ramóna', 'ramona@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', '025592545', 'user', '2023-02-18', '2023-04-04', NULL, 'ramona'),
 (4, 'admin', 'admin@nudentist.com', '7af2d10b73ab7cd8f603937f7697cb5fe432c7ff', '+36205988683', 'admin', '2023-02-19', '2023-03-27', NULL, 'admin'),
 (5, 'Szabó Péter', 'szpeter20@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', '+3620584561825', 'user', '2023-03-03', '2023-03-21', 6, 'szabopetra'),
-(6, 'Zámbó Illés', 'zamboilles@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', '+36205988683', 'user', '2023-03-03', '2023-04-03', 8, 'Zambo3'),
-(7, 'Foki Zoltán', 'turrfz@turr.hu', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', '+36125525235652', 'user', '2023-03-03', '2023-03-27', NULL, 'fokizoltan'),
+(6, 'Zámbó Illés', 'zamboilles@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', '0691132401', 'user', '2023-03-03', '2023-04-12', 8, 'Zambo3'),
+(7, 'Foki Zoltán', 'turrfz@turr.hu', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', '+36125525235652', 'user', '2023-03-03', '2023-04-05', NULL, 'fokizoltan'),
 (11, 'fiszfoszxd70', 'fiszfosz.0000@00000000.0000000000', '3810f3c42b21d2bc4f26609152909346c5a04923', '+3ddddddd6xddd', 'user', '2023-03-20', '0000-00-00', NULL, 'admin'),
 (12, 'Vigh Ákos', 'vighakos@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', '+3620696969', 'user', '2023-03-21', '2023-03-21', NULL, 'admin'),
 (13, 'Baranyi Dániel', 'baranyidaniel@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', '+3620696969', 'user', '2023-03-21', '2023-03-21', NULL, 'admin');
@@ -291,7 +293,7 @@ ALTER TABLE `ertekelesek`
 -- AUTO_INCREMENT a táblához `idopontok`
 --
 ALTER TABLE `idopontok`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT a táblához `kezelesek`
