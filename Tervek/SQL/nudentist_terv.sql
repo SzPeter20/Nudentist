0e5b2495ca65633724ff21bde8c286ccffcae95c
zamboilles21 <zamboilles@turr.hu>
2023-04-18 09:37:22 +0200

    Dokumentáció

--- Tervek/SQL/nudentist_terv.sql
+++ Tervek/SQL/nudentist_terv.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2023. Ápr 17. 09:34
+-- Létrehozás ideje: 2023. Ápr 18. 09:21
 -- Kiszolgáló verziója: 10.4.6-MariaDB
 -- PHP verzió: 7.3.8
 
@@ -45,7 +45,8 @@ CREATE TABLE `ertekelesek` (
 INSERT INTO `ertekelesek` (`ID`, `orvosID`, `paciensID`, `szoveges`, `csillagok`) VALUES
 (1, 6, 5, 'cunci az Ákos', 5),
 (2, 3, 6, '', 4),
-(3, 4, 6, '', 5);
+(3, 4, 6, '', 5),
+(4, 1, 6, 'Nagyon jól végezte a kezelést!', 5);
 
 -- --------------------------------------------------------
 
@@ -89,12 +90,12 @@ INSERT INTO `idopontok` (`ID`, `orvosID`, `idopont`, `datum`, `paciensID`, `nev`
 (23, 4, '15:54:00', '2023-03-15', 7, 'Foki Zoltán', 'turrfz@turr.hu', '+36125525235652', 'Elfogadva'),
 (24, 4, '10:00:00', '2023-03-25', 7, 'Foki Zoltán', 'turrfz@turr.hu', '+36125525235652', 'Elfogadásra vár'),
 (25, 1, '10:05:00', '2023-03-10', 7, 'Foki Zoltán', 'turrfz@turr.hu', '+36125525235652', 'Elfogadva'),
-(26, 1, '12:00:00', '2023-03-30', 6, 'Zámbó Illés', 'zamboilles@gmail.com', '+36205988683', 'Elfogadásra vár'),
+(26, 1, '12:00:00', '2023-03-30', 6, 'Zámbó Illés', 'zamboilles@gmail.com', '+36205988683', 'Elfogadva'),
 (27, 4, '13:07:00', '2023-03-29', 6, 'Zámbó Illés', 'zamboilles@gmail.com', '+36205988683', 'Elfogadva'),
 (28, 4, '12:59:00', '2023-04-19', 6, 'Zámbó Illés', 'zamboilles@gmail.com', '+36205988683', 'Elfogadásra vár'),
 (29, 4, '12:01:00', '2023-04-01', 6, 'Zámbó Illés', 'zamboilles@gmail.com', '+36205988683', 'Elfogadásra vár'),
 (30, 4, '08:01:00', '2023-04-01', 6, 'Zámbó Illés', 'zamboilles@gmail.com', '+36205988683', 'Elfogadásra vár'),
-(31, 1, '11:28:00', '2023-03-29', 6, 'Zámbó Illés', 'zamboilles@gmail.com', '0691132401', 'Elfogadásra vár');
+(31, 1, '11:28:00', '2023-03-29', 6, 'Zámbó Illés', 'zamboilles@gmail.com', '0691132401', 'Elfogadva');
 
 -- --------------------------------------------------------
 
@@ -106,7 +107,7 @@ CREATE TABLE `kezelesek` (
   `ID` int(11) NOT NULL,
   `neve` varchar(100) COLLATE utf8_hungarian_ci NOT NULL,
   `ar` int(11) NOT NULL,
-  `leiras` varchar(500) COLLATE utf8_hungarian_ci NOT NULL,
+  `leiras` varchar(1000) COLLATE utf8_hungarian_ci NOT NULL,
   `kategoria` varchar(50) COLLATE utf8_hungarian_ci NOT NULL,
   `kep` varchar(100) COLLATE utf8_hungarian_ci DEFAULT NULL,
   `logo` varchar(250) COLLATE utf8_hungarian_ci NOT NULL
@@ -116,14 +117,15 @@ CREATE TABLE `kezelesek` (
 -- A tábla adatainak kiíratása `kezelesek`
 --
 
-INSERT INTO `kezelesek` (`ID`, `neve`, `ar`, `leiras`, `kategoria`, `kep`) VALUES
-(1, 'Próba', 5000, 'próba', 'Próbakategória', 'dental-drill.jpg'),
-(2, 'Periapikális röntgen', 10000, 'Az ún. periapikális (foggyökércsúcs körüli) röntgenfelvétellel a fog koronájának legfelső pontjától egészen a gyökér legalsó pontjáig mindent láthatunk, a fogat megtámasztó csontállománnyal együtt.', 'Röntgen', 'peri.jpg'),
-(3, 'Korona', 5000, 'A korona egyetlen fog helyreállítására szolgál. Amennyiben a helyreállítandó fog koronai része oly mértékben destruálódott, hogy azt töméssel vagy betéttel nem lehet helyreállítani, javasolt a korona készítése. Ha egy fog már elvesztésre került, implantátumre készülő korona lehet a megoldás.', 'Pótlás', 'Korona.jpg'),
-(4, 'Ideiglenes fogtömés', 3500, 'Az ideiglenes tömés – ahogy a nevében is benne van – csak egy rövid ideig tartó, áthidaló ellátás. Amíg el nem készül a végleges fogtömés, a fogorvos ezzel az ideiglenes megoldással látja el a fogat.', 'Pótlás', 'fogtomes-utan.jpg'),
-(5, 'Nyitott kürett foganként', 45000, 'Lényege ennek a kezelésnek is az, hogy helyreállítsuk a fog rögzítő apparátus megfelelő kapcsolatát. A beavatkozást lehet foganként, valamint kvadránsokként (7-8 fog) végezni, a fogágybetegség mértékétől függően.\r\n', 'Parodontológia', 'tasakKezeles.png'),
-(6, 'Csontpótlás', 49500, 'Fogorvosi tekintetben csontpótlásról több esetben beszélhetünk, kezdve az implantációs beavatkozásokat megelőző, a csont térfogatát növelő ún. augmentációs beavatkozásoktól a regeneratív, új csont képződésével járó parodontális műtétekig.\r\nA különböző szituációk és célok más-más eljárást, technikát és csontpótló anyagot is kívánnak. Az alábbiakban ezeket vesszük sorra', 'Szájsebészet', 'csontpot.png'),
-(7, 'Láthatatlan fogszabályozás', 299900, 'desc', 'Láthatatlanok', 'lathatatlan.jpg');
+INSERT INTO `kezelesek` (`ID`, `neve`, `ar`, `leiras`, `kategoria`, `kep`, `logo`) VALUES
+(1, 'Próba', 5000, 'próba', 'Próbakategória', 'dental-drill.jpg', ''),
+(2, 'Periapikális röntgen', 10000, 'Az ún. periapikális (foggyökércsúcs körüli) röntgenfelvétellel a fog koronájának legfelső pontjától egészen a gyökér legalsó pontjáig mindent láthatunk, a fogat megtámasztó csontállománnyal együtt.', 'Röntgen', 'peri.jpg', ''),
+(3, 'Korona', 5000, 'A korona egyetlen fog helyreállítására szolgál. Amennyiben a helyreállítandó fog koronai része oly mértékben destruálódott, hogy azt töméssel vagy betéttel nem lehet helyreállítani, javasolt a korona készítése. Ha egy fog már elvesztésre került, implantátumre készülő korona lehet a megoldás.', 'Pótlás', 'Korona.jpg', ''),
+(4, 'Ideiglenes fogtömés', 3500, 'Az ideiglenes tömés – ahogy a nevében is benne van – csak egy rövid ideig tartó, áthidaló ellátás. Amíg el nem készül a végleges fogtömés, a fogorvos ezzel az ideiglenes megoldással látja el a fogat.', 'Pótlás', 'fogtomes-utan.jpg', ''),
+(5, 'Nyitott kürett foganként', 45000, 'Lényege ennek a kezelésnek is az, hogy helyreállítsuk a fog rögzítő apparátus megfelelő kapcsolatát. A beavatkozást lehet foganként, valamint kvadránsokként (7-8 fog) végezni, a fogágybetegség mértékétől függően.\r\n', 'Parodontológia', 'tasakKezeles.png', ''),
+(6, 'Csontpótlás', 49500, 'Fogorvosi tekintetben csontpótlásról több esetben beszélhetünk, kezdve az implantációs beavatkozásokat megelőző, a csont térfogatát növelő ún. augmentációs beavatkozásoktól a regeneratív, új csont képződésével járó parodontális műtétekig.\r\nA különböző szituációk és célok más-más eljárást, technikát és csontpótló anyagot is kívánnak. Az alábbiakban ezeket vesszük sorra', 'Szájsebészet', 'csontpot.png', ''),
+(7, 'Láthatatlan fogszabályozás', 495000, 'Az új innovációnak köszönhetően a precíziós 3D tervezés és gyártás segítségével maximális pontossággal a fogakra illeszthető, láthatatlan sínt kap, ami kényelmes, viselése nem jár fájdalommal, nem befolyásolja az étrendet és a komfortérzetet. A kezelés hossza lényegesen rövidebb, mint más, hagyományos eljárással zajló kezelések időtartama!', 'Láthatatlanok', 'lathatatlan.jpg', ''),
+(8, 'Gingivectomia/ínyplasztika (foganként)', 15000, 'Az íny sebészeti eltávolítását gingivectomiának (ínyszövetnek) nevezik. Amikor az íny eltávolodott a fogaktól, mély zsebeket produkálva, gingivectomiára van szükség. A plakkot és a fogkőt a zsebek miatt nehéz eltávolítani. A gingivectomiát általában azelőtt végzik, hogy az ínybetegség károsította volna a fogakat tartó csontot.Az ínyplasztika egy másik kifejezés a parodontális műtétre. Az ínyplasztika abban különbözik a gingivectomiától, hogy az előbbi csak az íny részleges kivágását tartalmazza (plasztika). Ez utóbbi kiküszöböli a gumi teljes területét. ', 'Parodontológia', 'gingivec.jpg', '');
 
 -- --------------------------------------------------------
 
@@ -153,14 +155,14 @@ CREATE TABLE `orvosok` (
 --
 
 INSERT INTO `orvosok` (`ID`, `nev`, `email`, `password`, `bemutatkozas`, `szakterulet`, `tanulmanyok`, `idopontID`, `kezelesekID`, `telefonszam`, `kep`, `last`, `reg`, `jogok`) VALUES
-(1, 'Kiss Tamás', 'kisstamas@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', 'Kiss Tamás profi fogorvos', 'Gyermek fogorvos', 'Fogorvos egyetem', 1, 1, '+36205365214', 'IbrahimLajos', '2023-04-17 09:28:25', '0000-00-00 00:00:00', 'doktor'),
-(2, 'Tamás Klára', 't.klara@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', 'Tamás Klára másik fogorvos', 'fogkő', 'Fogorvos egyetem', 3, 2, '06323512612', 'LajosneKlara', '2023-03-06 12:54:03', '0000-00-00 00:00:00', 'doktor'),
-(3, 'Ifjabb Kiss Pista Tamás ', 'IGFPN@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', 'Tamás a legprofibb fogszabályzókkal foglalkozó orvos', 'Fogkőfényesítő', 'Fogorvos egyetem', 1, 3, '616546462484', 'Asztalossimon', '2023-03-06 12:54:03', '0000-00-00 00:00:00', 'doktor'),
-(4, 'Kerék Tomázia Éva ', 'k.teve@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', 'Világelső fogorvos', 'Minden is', 'Fogorvos egyetem', 2, 4, '+362045963211', 'kerektomazia', '2023-04-03 13:07:39', '0000-00-00 00:00:00', 'doktor'),
-(5, 'Zámbó Ilona', 'zamboilona89@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', 'Sziasztok ', 'Szájhigénia', 'Fogorvos egyetem', 1, 5, '0696969696', 'zamboilona', '2023-03-14 09:20:40', '2023-03-20 12:31:00', 'doktor'),
-(6, 'Szomorú Ágnes', 'dr.szomoruagnes@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', 'Sziasztok', 'Fogtechnikus', 'Fogorvos egyetem', 2, 6, '0620584212', 'szomoruagnes', '2023-03-14 09:27:22', '2023-03-13 14:09:00', 'doktor'),
-(7, 'Simonfi Tamás', 'simifi@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', 'Sziasztok', 'Általános fogorvos\r\n', 'Fogorvos egyetem', 4, 7, '0620458598', 'SimonfiTamas', '2023-03-14 09:32:17', '2023-03-13 14:19:00', 'doktor'),
-(8, 'Szabó Petra', 'szpetra20@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', 'Sziasztok', 'Fogorvos asszisztens', 'Fogorvos egyetem', 2, 3, '06923145812515', 'szabopetra', '2023-03-28 09:02:18', '2023-03-17 11:00:00', 'doktor'),
+(1, 'Kiss Tamás', 'kisstamas@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', 'Kiss Tamás profi fogorvos', 'Gyermek- és ifjúsági fogász', 'Semmelweis Egyetem Fogorvostudományi Kar', 1, 1, '+36205365214', 'IbrahimLajos', '2023-04-17 13:22:15', '0000-00-00 00:00:00', 'doktor'),
+(2, 'Tamás Klára', 't.klara@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', 'Tamás Klára másik fogorvos', 'Szájsebész', 'Szegedi Tudományegyetem - Fogorvostudományi Kar', 3, 2, '06323512612', 'LajosneKlara', '2023-03-06 12:54:03', '0000-00-00 00:00:00', 'doktor'),
+(3, 'Ifjabb Kiss Pista Tamás ', 'IGFPN@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', 'Tamás a legprofibb fogszabályzókkal foglalkozó orvos', 'Dentálhigiénikus', 'Fogászati és Szájsebészeti Oktató Intézet, Semmelweis Egyetem', 1, 3, '616546462484', 'Asztalossimon', '2023-03-06 12:54:03', '0000-00-00 00:00:00', 'doktor'),
+(4, 'Kerék Tomázia Éva ', 'k.teve@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', 'Világelső fogorvos', 'Fogszakorvos', 'Semmelweis Egyetem Fogorvostudományi Kar', 2, 4, '+362045963211', 'kerektomazia', '2023-04-18 09:17:37', '0000-00-00 00:00:00', 'doktor'),
+(5, 'Zámbó Ilona', 'zamboilona89@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', 'Sziasztok ', ' Parodontológus', 'Szegedi Tudományegyetem - Fogorvostudományi Kar', 1, 5, '0696969696', 'zamboilona', '2023-03-14 09:20:40', '2023-03-20 12:31:00', 'doktor'),
+(6, 'Szomorú Ágnes', 'dr.szomoruagnes@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', 'Sziasztok', 'Szájsebész', 'Semmelweis Egyetem Fogorvostudományi Kar', 2, 6, '0620584212', 'szomoruagnes', '2023-03-14 09:27:22', '2023-03-13 14:09:00', 'doktor'),
+(7, 'Simonfi Tamás', 'simifi@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', 'Sziasztok', 'Általános fogorvos\r\n', 'Szegedi Tudományegyetem - Fogorvostudományi Kar', 4, 7, '0620458598', 'SimonfiTamas', '2023-03-14 09:32:17', '2023-03-13 14:19:00', 'doktor'),
+(8, 'Szabó Petra', 'szpetra20@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', 'Sziasztok', 'Fogorvos asszisztens', 'Semmelweis Egyetem Fogorvostudományi Kar', 2, 3, '06923145812515', 'szabopetra', '2023-03-28 09:02:18', '2023-03-17 11:00:00', 'doktor'),
 (9, 'Lakatos Viktoria', 'lak.vik@gmail.com', '-', 'Sziasztok', 'Takarító', '', NULL, NULL, '', 'lakatosviktoria', '2023-03-17 11:08:04', '2023-03-17 11:00:00', '-'),
 (10, 'Csongrádi Daniella', 'csongiD@gmail.com', '-', 'Sziasztok', 'Pénzügyi tanácsadó ', 'Budapesti Gazdasági Egyetem', NULL, NULL, '+36206945221', 'csongradidaniela', '2023-03-17 11:14:04', '2023-03-17 12:00:00', '-'),
 (11, 'Féreg Julianna', 'fefejul@gmail.com', '-', 'Egy féreg vagyok', 'Humán erőforrás', 'SOTER-LINE', NULL, NULL, '+25814581455', 'feregjulianna', '2023-03-17 11:16:44', '2023-03-17 14:00:00', '-'),
@@ -196,7 +198,7 @@ INSERT INTO `users` (`ID`, `nev`, `email`, `password`, `telefonszam`, `jogok`, `
 (3, 'Ramóna', 'ramona@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', '025592545', 'user', '2023-02-18', '2023-04-04', NULL, 'ramona'),
 (4, 'admin', 'admin@nudentist.com', '7af2d10b73ab7cd8f603937f7697cb5fe432c7ff', '+36205988683', 'admin', '2023-02-19', '2023-03-27', NULL, 'admin'),
 (5, 'Szabó Péter', 'szpeter20@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', '+3620584561825', 'user', '2023-03-03', '2023-03-21', 6, 'szabopetra'),
-(6, 'Zámbó Illés', 'zamboilles@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', '+36205988683', 'user', '2023-03-03', '2023-04-17', 8, 'Zambo3'),
+(6, 'Zámbó Illés', 'zamboilles@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', '+36205988683', 'user', '2023-03-03', '2023-04-18', 8, 'Zambo3'),
 (7, 'Foki Zoltán', 'turrfz@turr.hu', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', '+36125525235652', 'user', '2023-03-03', '2023-03-27', NULL, 'fokizoltan'),
 (11, 'fiszfoszxd70', 'fiszfosz.0000@00000000.0000000000', '3810f3c42b21d2bc4f26609152909346c5a04923', '+3ddddddd6xddd', 'user', '2023-03-20', '0000-00-00', NULL, 'admin'),
 (12, 'Vigh Ákos', 'vighakos@gmail.com', '170bec8a686f790c7eb8dfa2fae3cc04d24394d2', '+3620696969', 'user', '2023-03-21', '2023-03-21', NULL, 'admin'),
@@ -229,7 +231,6 @@ INSERT INTO `uzenetek` (`ID`, `senderID`, `senderType`, `recipientID`, `recipien
 (2, 0, '', 0, '', '', NULL, '0000-00-00', 'olvasatlan'),
 (3, 0, '', 0, '', '', NULL, '0000-00-00', 'olvasatlan'),
 (4, 5, 'user', 2, 'doktor', '245gtrh', 'ethtrhtr', '2023-03-20', 'olvasatlan'),
-(6, 1, 'doktor', 6, 'user', 'sdafs', 'scdasda', '2023-03-22', 'olvasott'),
 (7, 6, 'user', 6, 'doktor', 'sdafs', 'mmhb , bvhmb  m', '2023-03-29', 'olvasatlan');
 
 --
@@ -289,7 +290,7 @@ ALTER TABLE `uzenetek`
 -- AUTO_INCREMENT a táblához `ertekelesek`
 --
 ALTER TABLE `ertekelesek`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT a táblához `idopontok`
@@ -301,7 +302,7 @@ ALTER TABLE `idopontok`
 -- AUTO_INCREMENT a táblához `kezelesek`
 --
 ALTER TABLE `kezelesek`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT a táblához `orvosok`
